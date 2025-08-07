4352f6d5a9dae7802d39c6d25a8f2704805343cd
zarlosquperman <jang8408@gw1.kr>
2025-08-07 16:13:21 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,4 +57,3 @@ st.image("https://www.streamlit.io/images/brand/streamlit-logo-primary-colormark
 # 7. 유튜브 링크 (썸네일 표시)
 st.write("유튜브 동영상 예제")
 st.video("https://www.youtube.com/watch?v=B2iAodr0fOo")
-
